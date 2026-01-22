78458627dea360083e36a1faf7256b003d6f5746
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-22 23:20:17 +0200

    Update db_draft.py

--- drafts/db_draft.py
+++ drafts/db_draft.py
@@ -2,24 +2,45 @@ import datetime
 import json
 import os
 import pathlib
+import random
+import time
 
 
 # We create a func that stores the timetable data for each client
 def db_start_draft(filepath, id):
-    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/db_draft.json"
+    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_timer_draft.json"
     if os.path.isfile(db_filepath) is True:
         with open(db_filepath, "r") as file:
-            all_clients_db = json.load(file)
+            client_db = json.load(file)
     else:
-        all_clients_db = []
+        client_db = {}
 
-    client_db = {
+    client_db_dict_start = {
         "ID": id,
         "Date & Hour": {"Intrare in parcare": str(datetime.datetime.now())},
     }
 
-    all_clients_db.append(client_db)
+    db_start_dict = client_db | client_db_dict_start
 
     with open(db_filepath, "w") as file:
         # indent=4 for better farmat
-        json.dump(all_clients_db, file, indent=4)
+        json.dump(db_start_dict, file, indent=4)
+
+
+def db_end_draft(filepath, id):
+    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_timer_draft.json"
+
+    ore = random.randint(2, 5)
+    t = ore
+    while t:
+        time.sleep(1)
+        t -= 1
+
+    with open(db_filepath, "r") as file:
+        client_db = json.load(file)
+
+    client_db["Date & Hour"]["Iesire din parcare"] = str(datetime.datetime.now())
+    client_db["Date & Hour"]["Timp"] = f"{ore} ore"
+    with open(db_filepath, "w") as file:
+        # indent=4 for better farmat
+        json.dump(client_db, file, indent=4)
