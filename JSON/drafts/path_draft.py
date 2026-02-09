adf5444742e73a6cbf76c7d4a321f25340674a4c
Petre Razvan <petre.ispir2002@protonmail.com>
2026-02-09 17:55:49 +0200

    sorted stuff

--- JSON/drafts/path_draft.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-filepath = "drafts/data/clients_draft.json"
-
-
-db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/db_draft.json"
-
-
-print(db_filepath)
--- JSON/drafts/timestamp_example.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import time
-
-start = datetime.datetime.now()
-time.sleep(5)
-end = datetime.datetime.now()
-print(end)
-
-print(end - start)
