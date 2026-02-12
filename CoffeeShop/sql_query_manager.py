9c3ef79d0643c7064d212937a633ed635507a7d3
Petre Razvan <petre.ispir2002@protonmail.com>
2026-02-12 20:47:30 +0200

    Update sql_query_manager.py

--- CoffeeShop/sql_query_manager.py
+++ CoffeeShop/sql_query_manager.py
@@ -1,15 +1,15 @@
 DB_PATH = "CoffeeShop/caffee/caffee.db"
 
-DRAFT_DB_PATH = "CoffeeShop/caffee/draft_db/caffee.db"
+TEST_DB_PATH = "CoffeeShop/test_db/caffee.db"
 
 EMPLOYEE_TABLE = "Employee"
 EMPLOYEE_TABLE_COLUMNS = "('start.date', 'role')"
-EMPLOYEE_VALUES_STARTDATE_ROLE = "(?, ?)"
+EMPLOYEE_STARTDATE_ROLE = "(?, ?)"
 
 MENU_TABLE = "Menu"
 MENU_TABLE_COLUMNS = "('name', 'price')"
-MENU_VALUES_NAME_PRICE = "(?, ?)"
+MENU_NAME_PRICE = "(?, ?)"
 
 ORDERS_TABLE = "Orders"
 ORDERS_TABLE_COLUMNS = "('name', 'qty', 'client_id','timestamp', 'casier_id')"
-ORDERS_VALUES_NAME_QTY_CLIENTID_TIMESTAMP_CASHIERID = "(?, ?, ?, ?, ?)"
+ORDERS_NAME_QTY_CLIENTID_TIMESTAMP_CASHIERID = "(?, ?, ?, ?, ?)"
